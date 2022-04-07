6b1ee289c3090fb362cdf8fe13b21fc629c9a250
Eduardo Díez Viñuela <eduardo.diez@idonial.com>
2022-04-07 09:32:09 +0200

    File scoped namespace in EventArgs

--- EventArgs.cs
+++ EventArgs.cs
@@ -1,54 +1,53 @@
 ﻿using System;
 using TTNet.Data.Model;
 
-namespace TTNet.Data
+namespace TTNet.Data;
+
+/// <summary>
+/// Event arguments for a generic mesage.
+/// </summary>
+public class MessageReceivedEventArgs : EventArgs
 {
     /// <summary>
-    /// Event arguments for a generic mesage.
+    /// MQTT topic.
     /// </summary>
-    public class MessageReceivedEventArgs : EventArgs
+    public string Topic { get; init; }
+
+    /// <summary>
+    /// MQTT topic fields.
+    /// </summary>
+    /// <value><see cref="TTNet.Data.MessageReceivedEventArgs.Topic"/> splitted.</value>
+    public string[] TopicFields { get; init; }
+
+    /// <summary>
+    /// Application identifier.
+    /// </summary>
+    public string AppID { get; init; }
+
+    /// <summary>
+    /// Tenant identifier.
+    /// </summary>
+    public string? TenantID { get; init; }
+
+    /// <summary>
+    /// Device identifier.
+    /// </summary>
+    public string DeviceID => TopicFields[3];
+
+
+    /// <summary>
+    /// The message received.
+    /// </summary>
+    public Message Message { get; init; }
+
+    internal MessageReceivedEventArgs(Message msg, string topic, string[] topicFields)
     {
-        /// <summary>
-        /// MQTT topic.
-        /// </summary>
-        public string Topic { get; init; }
-
-        /// <summary>
-        /// MQTT topic fields.
-        /// </summary>
-        /// <value><see cref="TTNet.Data.MessageReceivedEventArgs.Topic"/> splitted.</value>
-        public string[] TopicFields { get; init; }
-
-        /// <summary>
-        /// Application identifier.
-        /// </summary>
-        public string AppID { get; init; }
-
-        /// <summary>
-        /// Tenant identifier.
-        /// </summary>
-        public string? TenantID { get; init; }
-
-        /// <summary>
-        /// Device identifier.
-        /// </summary>
-        public string DeviceID => TopicFields[3];
-
-
-        /// <summary>
-        /// The message received.
-        /// </summary>
-        public Message Message { get; init; }
-
-        internal MessageReceivedEventArgs(Message msg, string topic, string[] topicFields)
-        {
-            string[] apptenant = topicFields[1].Split('@');
-            Message = msg;
-            Topic = topic;
-            TopicFields = topicFields;
-            AppID = apptenant[0];
-            if (apptenant.Length > 1)
-                TenantID = apptenant[1];
-        }
+        string[] apptenant = topicFields[1].Split('@');
+        Message = msg;
+        Topic = topic;
+        TopicFields = topicFields;
+        AppID = apptenant[0];
+        if (apptenant.Length > 1)
+            TenantID = apptenant[1];
     }
 }
